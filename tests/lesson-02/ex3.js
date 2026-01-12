6cc6344bb9330d27df50e6b3dcb96b707abdb44b
AnhNTV75 <Vanhnguyen1995@gmail.com>
2026-01-12 21:10:16 +0700

    btvn-lesson-2

---
+++ tests/lesson-02/ex3.js
@@ -0,0 +1,10 @@
+const number = 10;
+if (number > 0) {
+    console.log("Giá trị input là số dương")
+}
+if (number < 0) {
+    console.log("Giá trị input là số âm")
+}
+if (number ===0) {
+    console.log("Giá trị nhập là số 0")
+}
---
+++ tests/lesson-02/ex4.js
@@ -0,0 +1,12 @@
+const height = 157
+const oddHeight = height - 100;
+const ideaWeight = oddHeight * 9 / 10;
+const maxWeight = oddHeight;
+let minWeight = oddHeight * 8 / 10;
+
+console.log(
+    "Cân nặng lý tưởng:", ideaWeight,
+    "||Cân nặng tối đa:", maxWeight,
+    "||Cân nặng tối thiểu:", minWeight
+
+);
